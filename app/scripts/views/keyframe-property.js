4992a09f277c7a98da70949e5ee476c9ead1c553
Jeremy Kahn <jeremyckahn@gmail.com>
2014-03-01 17:42:11 -0600

    Adds keyframeProperty view drag handlers to change their position within the timeline.

--- app/scripts/views/keyframe-property.js
+++ app/scripts/views/keyframe-property.js
@@ -5,6 +5,8 @@ define([
   ,'backbone'
   ,'mustache'
 
+  ,'rekapi.timeline.constants'
+
   ,'text!templates/keyframe-property.mustache'
 
 ], function (
@@ -13,6 +15,8 @@ define([
   ,_
   ,Backbone
   ,Mustache
+
+  ,rekapiTimelineConstants
 
   ,KeyframePropertyTemplate
 
@@ -35,12 +39,39 @@ define([
       this._$keyframeProperty = this.$el.find('.keyframe-property');
       this._$keyframeProperty.dragon({
         within: this.keyframePropertyTrackView.$el
+        ,drag: _.bind(this.onDrag, this)
       });
     }
 
     ,render: function () {
       this.$el.html(
           Mustache.render(KeyframePropertyTemplate, this.keyframeProperty));
+    }
+
+    ,onDrag: function () {
+      this.updateKeyframeProperty();
+    }
+
+    /**
+     * Reads the state of the UI and persists that to the Rekapi animation.
+     */
+    ,updateKeyframeProperty: function () {
+      var $container = this.keyframePropertyTrackView.$el;
+      var distanceFromLeft = this._$keyframeProperty.offset().left
+          - $container.offset().left
+          - parseInt($container.css('border-left-width'), 10)
+          - parseInt($container.css('padding-left'), 10);
+
+      var scaledValue = (
+          distanceFromLeft / rekapiTimelineConstants.PIXELS_PER_SECOND) * 1000;
+
+      // Modify the keyframeProperty via its actor so that the state of the
+      // animation is updated.
+      var keyframeProperty = this.keyframeProperty;
+      keyframeProperty.actor.modifyKeyframeProperty(
+          keyframeProperty.name, keyframeProperty.millisecond, {
+            millisecond: scaledValue
+          });
     }
   });
 
