9d4368164bb0879c4478f75b30beec6b93caadb7
Jeremy Kahn <jeremyckahn@gmail.com>
2014-03-10 20:23:03 -0500

    Adds basic play/pause/stop UI controls.

--- app/scripts/views/control-bar.js
+++ app/scripts/views/control-bar.js
@@ -14,18 +14,36 @@ define([
   'use strict';
 
   var ControlBarView = Backbone.View.extend({
+    events: {
+      'click .rekapi-timeline-play': 'onClickPlay'
+      ,'click .rekapi-timeline-pause': 'onClickPause'
+      ,'click .rekapi-timeline-stop': 'onClickStop'
+    }
+
     /**
      * @param {Object}
      *   @param {RekapiTimeline} rekapiTimeline
      *   @param {HTMLElement} el
      */
-    initialize: function (opts) {
+    ,initialize: function (opts) {
       this.rekapiTimeline = opts.rekapiTimeline;
       this.render();
     }
 
     ,render: function () {
       this.$el.html(controlBarTemplate);
+    }
+
+    ,onClickPlay: function () {
+      this.rekapiTimeline.rekapi.play();
+    }
+
+    ,onClickPause: function () {
+      this.rekapiTimeline.rekapi.pause();
+    }
+
+    ,onClickStop: function () {
+      this.rekapiTimeline.rekapi.stop();
     }
   });
 
