7d00874f47fa504200712deb129ee820f05a59c0
Jeremy Kahn <jeremyckahn@gmail.com>
2014-03-10 20:30:39 -0500

    Hides and shows playback buttons appropriately.

--- app/scripts/views/control-bar.js
+++ app/scripts/views/control-bar.js
@@ -28,6 +28,10 @@ define([
     ,initialize: function (opts) {
       this.rekapiTimeline = opts.rekapiTimeline;
       this.render();
+
+      if (this.rekapiTimeline.rekapi.isPlaying()) {
+        this.$el.addClass('rekapi-timeline-playing');
+      }
     }
 
     ,render: function () {
@@ -36,14 +40,19 @@ define([
 
     ,onClickPlay: function () {
       this.rekapiTimeline.rekapi.play();
+      this.$el.addClass('rekapi-timeline-playing');
     }
 
     ,onClickPause: function () {
       this.rekapiTimeline.rekapi.pause();
+      this.$el.removeClass('rekapi-timeline-playing');
     }
 
     ,onClickStop: function () {
-      this.rekapiTimeline.rekapi.stop();
+      this.rekapiTimeline.rekapi
+        .stop()
+        .update(0);
+      this.$el.removeClass('rekapi-timeline-playing');
     }
   });
 
