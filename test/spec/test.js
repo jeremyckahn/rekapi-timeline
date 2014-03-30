65bbc6db765ab5f988bfc818cf19143389ac4c49
Jeremy Kahn <jeremyckahn@gmail.com>
2014-03-30 16:47:08 -0500

    Loads test app code in the browser.

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,55 @@
-/* global describe, it */
+/* global assert, describe, mocha, it */
+require.config({
+  baseUrl: 'scripts'
+  ,shim: {
+    underscore: {
+      exports: '_'
+    }
+    ,backbone: {
+      deps: [
+        'underscore'
+        ,'jquery'
+      ]
+      ,exports: 'Backbone'
+    }
+    ,'jquery-dragon': {
+      deps: [
+        'jquery'
+      ]
 
-(function () {
+    }
+  }
+  ,paths: {
+    jquery:           '../bower_components/jquery/dist/jquery'
+    ,'jquery-dragon':  '../bower_components/jquery-dragon/src/jquery.dragon'
+    ,backbone:        '../bower_components/backbone/backbone'
+    ,underscore:      '../bower_components/underscore/underscore'
+    ,mustache:        '../bower_components/mustache/mustache'
+    ,text:            '../bower_components/requirejs-text/text'
+    ,rekapi:          '../bower_components/rekapi/dist/rekapi'
+    ,shifty:          '../bower_components/shifty/dist/shifty'
+  }
+});
+
+require([
+
+  'rekapi'
+  ,'rekapi.timeline'
+
+], function (
+
+  Rekapi
+
+) {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-
-      });
+  describe('The code loads', function () {
+    it('should define Rekapi.prototype.createTimeline', function () {
+      assert.isFunction(Rekapi.prototype.createTimeline);
     });
   });
-})();
+
+  mocha.run();
+
+});
+
