9e3a8356f50ad2582624c06559f5a6df98787b40
Jeremy Kahn <jeremyckahn@gmail.com>
2015-03-08 16:31:50 -0500

    Fix tests.

--- test/main.js
+++ test/main.js
@@ -24,6 +24,8 @@ require.config({
   ,paths: {
     jquery:           '../bower_components/jquery/dist/jquery'
     ,'jquery-dragon':  '../bower_components/jquery-dragon/src/jquery.dragon'
+    ,'jquery-mousewheel':
+        '../bower_components/jquery-mousewheel/jquery.mousewheel'
     ,backbone:        '../bower_components/backbone/backbone'
     ,underscore:      '../bower_components/underscore/underscore'
     ,mustache:        '../bower_components/mustache/mustache'
@@ -32,6 +34,17 @@ require.config({
     ,shifty:          '../bower_components/shifty/dist/shifty'
     ,chai:            '../../test/bower_components/chai/chai'
     ,mocha:           '../../test/bower_components/mocha/mocha'
+
+    // jshint maxlen: 120
+    // jck-library-extensions
+    ,'auto-update-textfield':
+        '../bower_components/jck-library-extensions/src/backbone/auto-update-textfield/auto-update-textfield'
+    ,'incrementer-field':
+        '../bower_components/jck-library-extensions/src/backbone/incrementer-field/incrementer-field'
+    ,tabs: '../bower_components/jck-library-extensions/src/backbone/tabs/tabs'
+    ,pane: '../bower_components/jck-library-extensions/src/backbone/pane/pane'
+    ,alert: '../bower_components/jck-library-extensions/src/backbone/alert/alert'
+    ,modal: '../bower_components/jck-library-extensions/src/backbone/modal/modal'
   }
 });
 
