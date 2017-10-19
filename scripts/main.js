21786ac17acf6eb1862c0acd8d2dda195a2cdd62
Jeremy Kahn <jeremyckahn@gmail.com>
2017-10-19 07:56:49 -0500

    Drop requirejs-text

--- scripts/main.js
+++ scripts/main.js
@@ -10,8 +10,7 @@ require.config({
     }
   }
   ,paths: {
-    text: 'node_modules/requirejs-text/text'
-    ,jquery: 'node_modules/jquery/dist/jquery'
+    jquery: 'node_modules/jquery/dist/jquery'
     ,'jquery-dragon': 'node_modules/jquery-dragon/src/jquery.dragon'
     ,backbone: 'node_modules/backbone/backbone'
     ,underscore: 'node_modules/lodash/index'
