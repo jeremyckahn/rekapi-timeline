eb3c74cd92028935acdaf9e89fff5cf638cb76fc
Jeremy Kahn <jeremyckahn@gmail.com>
2017-11-03 16:41:25 -0500

    Put top-level components together

--- src/rekapi-timeline.js
+++ src/rekapi-timeline.js
@@ -1,7 +1,20 @@
 import React, { Component } from 'react';
+import Details from './details';
+import Timeline from './timeline';
+import BottomFrame from './bottom-frame';
 
 export default class RekapiTimeline extends Component {
+  constructor (props) {
+    super(props);
+  }
+
   render () {
-    return <div className="rekapi-timeline"></div>;
+    return (
+      <div className="rekapi-timeline">
+        <Details />
+        <Timeline />
+        <BottomFrame />
+      </div>
+    );
   }
 }
