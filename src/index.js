36cd26a7b0ab2d2837275b2dc0a57fb7d70c17f5
Jeremy Kahn <jeremyckahn@gmail.com>
2017-11-27 08:01:20 -0600

    Set up style loading system

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
+import './styles/index.sass';
 export { RekapiTimeline } from './rekapi-timeline';
--- webpack.test.config.js
+++ webpack.test.config.js
@@ -19,6 +19,21 @@ module.exports = {
         test: /\.js$/,
         use: 'babel-loader',
         exclude: path.join(__dirname, 'node_modules')
+      }, {
+        test: /\.(sass|scss|css)$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+            includePaths: [
+              path.resolve(__dirname, './node_modules/compass-mixins/lib')
+            ]
+          }
+        }]
       }
     ]
   },
