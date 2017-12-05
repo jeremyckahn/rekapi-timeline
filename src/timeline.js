cf7eb008bc220507ab313817424c4fdab71340e5
Jeremy Kahn <jeremyckahn@gmail.com>
2017-12-05 08:02:08 -0600

    WIP port of scrubber markup

--- src/timeline.js
+++ src/timeline.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+// FIXME: The .timeline-wrapper div will need the auto-resizing logic ported
+// from the original verison, but that should wait until the nested components
+// are implement as there may be better approaches to use than inline style
+// updates.
+//
+// Additionally, there is quite a bit of weird JavaScript-powered styling that
+// needs to be ported/adapted from the original version.
+
 const Timeline = props => (
-  <div className="timeline">
+  <div className="fill timeline">
+    <div className="timeline-wrapper">
+
+      <div className="scrubber">
+        <div className="scrubber-wrapper">
+          <div
+            className="scrubber-handle"
+            draggable="false"
+          >
+            <i className="glyphicon glyphicon-chevron-down scrubber-icon">&nbsp;</i>
+            <figure className="scrubber-guide"></figure>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>);
 
 export default Timeline;
