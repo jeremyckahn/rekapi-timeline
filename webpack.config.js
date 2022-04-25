08bbdb7d4f43817bbd4732df8ea062ef2c58c221
Jeremy Kahn <jeremyckahn@gmail.com>
2022-04-24 21:44:24 -0500

    chore: update dev dependencies for Mantra compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,33 @@
-const path = require('path');
-const Webpack = require('webpack');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path')
+const Webpack = require('webpack')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
-const { version } = require('./package.json');
+const { version } = require('./package.json')
 
-const rootDir = modulePath => path.resolve(__dirname, modulePath);
+const rootDir = modulePath => path.resolve(__dirname, modulePath)
 
 module.exports = {
   entry: {
     demo: './scripts/demo.js',
     'rekapi-timeline': './scripts/rekapi-timeline.js',
   },
+  mode: 'production',
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].js',
     library: 'rekapi-timeline',
     libraryTarget: 'umd',
-    umdNamedDefine: true
+    umdNamedDefine: true,
   },
   devtool: 'source-map',
   resolveLoader: {
     alias: {
-      text: 'raw-loader'
-    }
+      text: 'raw-loader',
+    },
   },
   resolve: {
-    modules: [
-      'node_modules',
-    ],
+    modules: ['node_modules'],
     symlinks: false,
     alias: {
       'jquery-dragon': rootDir('node_modules/jquery-dragon/src/jquery.dragon'),
@@ -37,8 +36,8 @@ module.exports = {
       lateralus: rootDir('node_modules/lateralus/dist/lateralus'),
       lodash: rootDir('node_modules/lodash/index.js'),
       rekapi: rootDir('node_modules/rekapi/src/main'),
-      underscore: 'lodash'
-    }
+      underscore: 'lodash',
+    },
   },
   module: {
     rules: [
@@ -50,45 +49,51 @@ module.exports = {
           rootDir('node_modules/shifty'),
           rootDir('node_modules/rekapi'),
           rootDir('node_modules/aenima'),
-          rootDir('node_modules/webpack-dev-server')
-        ]
-      }, {
+          rootDir('node_modules/webpack-dev-server'),
+        ],
+      },
+      {
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        use: [{
-          loader: 'url-loader'
-        }]
-      }, {
+        use: [
+          {
+            loader: 'url-loader',
+          },
+        ],
+      },
+      {
         test: /\.(sass|scss|css)$/,
-        use: [{
-          loader: 'style-loader'
-        }, {
-          loader: 'css-loader'
-        }, {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true,
-            includePaths: [
-              rootDir('node_modules/compass-mixins/lib')
-            ]
-          }
-        }]
-      }
-    ]
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              sassOptions: {
+                includePaths: [rootDir('node_modules/compass-mixins/lib')],
+              },
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
-    new CleanWebpackPlugin([ 'dist' ]),
-    new CopyWebpackPlugin([
-      { from: 'index.html' }
-    ]),
-    new Webpack.BannerPlugin(version)
+    new CleanWebpackPlugin(['dist']),
+    new CopyWebpackPlugin([{ from: 'index.html' }]),
+    new Webpack.BannerPlugin(version),
   ],
   devServer: {
     port: 9013,
     client: {
       overlay: {
         errors: true,
-        warnings: false
-      }
-    }
-  }
-};
+        warnings: false,
+      },
+    },
+  },
+}
